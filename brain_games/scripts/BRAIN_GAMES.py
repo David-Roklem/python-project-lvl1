83a6b0d519c7b0a7446d651de478a2104e047f92
David-Roklem <roklem@mail.ru>
2022-06-26 17:02:05 +0300

    step 3

--- brain_games/scripts/BRAIN_GAMES.py
+++ brain_games/scripts/BRAIN_GAMES.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-def main():
+def greet():
     print('Welcome to the Brain Games!')
+
+def main():
+    greet()
 
 if __name__ == '__main__':
     main()
 
 from brain_games.cli import welcome_user
+
