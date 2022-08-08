07a77e202bc8813968bf758bd0e948e52cbaeaf4
David-Roklem <roklem@mail.ru>
2022-08-08 18:51:24 +0300

    refactoring third time

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,5 +1,6 @@
 from random import randint
 from random import choice
+from operator import add, sub, mul
 
 
 GUIDE = 'What is the result of the expression?'
@@ -9,6 +10,12 @@ def play_calc_game():
     operators = ['+', '-', '*']
     first_operand = randint(1, 100)
     second_operand = randint(1, 100)
-    question = f'{first_operand} {choice(operators)} {second_operand}'
-    correct_answer = str(eval(question))
+    random_operator = choice(operators)
+    if random_operator == '+':
+        correct_answer = str(add(first_operand, second_operand))
+    elif random_operator == '-':
+        correct_answer = str(sub(first_operand, second_operand))
+    else:
+        correct_answer = str(mul(first_operand, second_operand))
+    question = f'{first_operand} {random_operator} {second_operand}'
     return question, correct_answer
