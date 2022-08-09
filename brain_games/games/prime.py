11659ee15a229c61a5726d8e6e4674e59460a207
David-Roklem <roklem@mail.ru>
2022-08-09 08:03:36 +0300

    refactoring third time

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,15 +7,14 @@ GUIDE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 # Создается вложенная функция,
 # которая проверяет, является ли число n простым или нет.
 def is_prime(n):
-    if n > 1:
-        if n % 2 == 0:
-            return n == 2
-        d = 3
-        while d * d <= n and n % d != 0:
-            d += 2
-        return d * d > n
-    else:
+    if n <= 1:
         return False
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
 
 
 def play_prime_game():
